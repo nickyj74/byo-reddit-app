350d937e830f0c9abe4a1e4d616ea63717381c2f
Nicky <134431774+nickyj74@users.noreply.github.com>
2023-11-04 18:50:59 +0000

    Commit following creation of first draft of app
    Some files modified, one created and several deleted... attempted commit from VSC direct to GitHub, but changes not showing in GitHub, despite showing on VSC.

---
+++ App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+/* MY CODE FOR ADDING TO TEST FILE, NEED TO TWEAK LOTS...
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+
+import '@testing-library/jest-dom';
+*/
