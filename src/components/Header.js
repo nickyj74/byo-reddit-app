686d0edef6eee1c6a920d92ae00b01292bcd49bf
Nicky <134431774+nickyj74@users.noreply.github.com>
2023-11-04 18:55:42 +0000

    Delete src/components/Header.js

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// Attempting to add in my first component for the search bar.
-export default function searchBar({ searchBar }) {
-    return (
-        <SearchBar />>
-        <link to="#">Search Bar</link>)
-}
