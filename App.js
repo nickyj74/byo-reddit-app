b269445d047144917a7a066ea7778d77ba2f8a7c
Nicky <134431774+nickyj74@users.noreply.github.com>
2023-11-05 16:26:08 +0000

    Updated and modified files
    App working with added error boundaries & css updates

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import Article from './components/Article';
+import './styles.css';
+
+//import ErrorBoundary from './components/ErrorBoundary';
 
 
 function App (){
   const [articles, setArticles] = useState([]);
   const [subreddit, setSubreddit] = useState("popular");
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch ("https://www.reddit.com/r/"+ subreddit +".json").then(res => {
-      if (res.status !== 200) {
-        console.log("ERROR");
-        return;
-      }
-
-      res.json().then(data => {
-        if (data !== null) {
+    setTimeout(() => {
+      fetch ("https://www.reddit.com/r/"+ subreddit +".json")
+        .then(res => {
+          if (!res.ok) {
+            throw Error("Error! Could not retrieve data!");
+          }
+          return res.json();
+        })
+        .then(data => {    
           setArticles(data.data.children);
-        }
-      });
+          setError(null);
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+        })
     })
   }, [subreddit]);
 
   return (
     <div className="App">
+      { error && <div> { error }</div>}
       <header className="App-header">
         <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)} />
       </header>
@@ -34,5 +45,6 @@ function App (){
     </div>
   );
 }
+
 
 export default App;
--- App.test.js
+++ App.test.js
@@ -6,13 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-/* MY CODE FOR ADDING TO TEST FILE, NEED TO TWEAK LOTS...
-
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-
-import '@testing-library/jest-dom';
-*/
